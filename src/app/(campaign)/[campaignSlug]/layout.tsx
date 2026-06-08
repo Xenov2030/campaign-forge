99c2a996a7086a46d7d3d81b43648c734f300744
FranQuarnolo <quarnolofrancisco@gmail.com>
2026-06-08 09:02:57 -0300

    feat(int): agregar Toaster de Sonner para notificaciones de campaña

--- src/app/(campaign)/[campaignSlug]/layout.tsx
+++ src/app/(campaign)/[campaignSlug]/layout.tsx
@@ -6,6 +6,7 @@ import { TopNav } from "@/components/layout/top-nav";
 import { DiceTray } from "@/components/dice/dice-tray";
 import { MasterAssistant } from "@/components/ai/master-assistant";
 import { CampaignRealtime } from "@/components/realtime/campaign-realtime";
+import { Toaster } from "sonner";
 
 interface CampaignLayoutProps {
   children: React.ReactNode;
@@ -96,6 +97,7 @@ export default async function CampaignLayout({ children, params }: CampaignLayou
         isMaster={isMaster}
         userId={user.id}
       />
+      <Toaster position="top-right" richColors />
       <DiceTray isMaster={isMaster} />
       {isMaster && (
         <MasterAssistant
